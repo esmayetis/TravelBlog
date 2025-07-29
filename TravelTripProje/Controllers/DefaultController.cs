29fb2752e7223e72a5a0ad2bc82cda96e5a0c4d1
Esma  Yetiş <100996071+esmayetis@users.noreply.github.com>
2025-07-29 23:54:25 +0300

    Proje dosyası ekle.

---
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+namespace TravelTripProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        Context c = new Context();
+		public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+			return View(degerler);
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public PartialViewResult Partial1()
+		{
+			var degerler = c.Blogs.OrderByDescending(x => x.Id).Take(2).ToList();
+
+			return PartialView(degerler);
+		}
+
+		public PartialViewResult Partial2()
+		{
+			var deger = c.Blogs.Where(x => x.Id == 1).ToList();
+			return PartialView(deger);
+		}
+	}
+}
---
+++ TravelTripProje/Models/Siniflar/Blog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Siniflar
+{
+	public class Blog
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Baslik { get; set; }
+		public DateTime Tarih { get; set; }
+
+		public string Aciklama { get; set; }
+		public string BlogImage { get; set; }
+		public ICollection<Yorumlar> Yorumlars { get; set; } // Bloga ait yorumları tutmak için - 1'e  çok ilişkisi
+
+		//bir bloğun birden fazla yorumu olabilir, bu yüzden ICollection kullanıyoruz.
+
+	}
+}
