5a69a54f198377eba4a29a5904a3594730e71854
Esma  Yetiş <100996071+esmayetis@users.noreply.github.com>
2025-07-31 23:52:07 +0300

    Admin sayfasını ekleme - yorum panelini düzenleme.

---
+++ TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+
+        Context c = new Context();
+		public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+			return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+		}
+
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);//yeniblog cshtml dosyasından gelen veriyi p parametresine atar
+			c.SaveChanges();
+            return RedirectToAction("Index");
+		}
+
+        public ActionResult BlogSil(int id)
+        {
+            var blog = c.Blogs.Find(id);
+            c.Blogs.Remove(blog);
+            c.SaveChanges();
+			return RedirectToAction("Index");
+        }
+
+        public ActionResult BlogGetir(int id)
+        {
+            var bl = c.Blogs.Find(id);
+            return View("BlogGetir", bl);
+		}
+
+        public ActionResult BlogGuncelle(Blog b)
+        {
+            var blog = c.Blogs.Find(b.Id);
+            blog.Baslik = b.Baslik;
+            blog.Aciklama = b.Aciklama;
+            blog.Tarih = b.Tarih;
+            blog.BlogImage = b.BlogImage;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+		}
+
+        public ActionResult YorumlarListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+		}
+	}
+}
--- TravelTripProje/Controllers/BlogController.cs
+++ TravelTripProje/Controllers/BlogController.cs
@@ -33,5 +33,20 @@ namespace TravelTripProje.Controllers
 
 			return View(by);
 		}
+
+		[HttpGet]
+		public PartialViewResult YorumYap(int id)
+		{
+			ViewBag.deger = id;
+			return PartialView();
+		}
+
+		[HttpPost]
+		public PartialViewResult YorumYap(Yorumlar y)
+        {
+			c.Yorumlars.Add(y);
+			c.SaveChanges();
+			return PartialView();
+		}
 	}
 }
